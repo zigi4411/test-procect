c6d869ed85574828c94b83e5c4c47f4487d50ddb
mareksibal <marek.sibal@akademiasovy.sk>
2016-11-14 10:07:23 +0100

    generator

---
+++ generator.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+#include<time.h>
+main(){
+int generator(int n){
+int *p=generator(7);
+int=3;
+int pole=malloc(sizeof(int);
+int i;
+int temp=vysledok;
+for(i=0;i<=3;i++);
+printf("%d",*temp++);
+printf("%d",sizeof(vysledok);
+free(vysledok);
+int *vyslegenerator(n);
+}
+}
